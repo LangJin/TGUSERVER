383bccade98b4ec504b12cd9613413f5cda24abd
LangJin <fenyukuang@163.com>
2020-07-15 17:33:41 +0800

    cors

--- app/errors.py
+++ app/errors.py
@@ -2,17 +2,22 @@
 __author__ = 'LangJin'
 from flask import Blueprint
 from .utils.retools import responsedata
+from werkzeug.exceptions import HTTPException
 
 
 errorbp = Blueprint("errorbp",__name__)
 
 
-@errorbp.app_errorhandler(404)
-def page_not_found(e):
-    return responsedata(msg="%s" % e),404
+class EigenException(HTTPException):
+    code = 500
+    description = '服务器异常，请联系管理员！'
+
 
 
 @errorbp.app_errorhandler(Exception)
 def internal_server_error(e):
-    status,error = 500, '%s(%s)' % (e.__class__.__name__, str(e))
+    if isinstance(e, HTTPException):
+        status, error = e.code,  e.description
+    else:
+        status,error = 500, '%s(%s)' % (e.__class__.__name__, str(e))
     return responsedata(msg=error),status
