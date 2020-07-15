3fc4f837510c0b1e583582490c96564e67344cff
LangJin <fenyukuang@163.com>
2020-07-15 11:56:48 +0800

    添加全局异常处理

--- app/errors.py
+++ app/errors.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 __author__ = 'LangJin'
 from flask import Blueprint
+from .utils.retools import responsedata
 
 
 errorbp = Blueprint("errorbp",__name__)
@@ -8,8 +9,10 @@ errorbp = Blueprint("errorbp",__name__)
 
 @errorbp.app_errorhandler(404)
 def page_not_found(e):
-    return "404", 404
+    return responsedata(msg="%s" % e),404
 
-@errorbp.app_errorhandler(500)
+
+@errorbp.app_errorhandler(Exception)
 def internal_server_error(e):
-    return "500", 500
+    status,error = 500, '%s(%s)' % (e.__class__.__name__, str(e))
+    return responsedata(msg=error),status
